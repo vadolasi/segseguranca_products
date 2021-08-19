5431e6af781e320500d0da43f240bda8d546d501
Vitor Daniel <vitor036daniel@gmail.com>
2021-08-19 13:21:00 -0300

    sla

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("http://segseguranca.com.br/base.asp?verif=ABCDEFGHI2.83321334405622POIUYT1.09861228866811&link=consulta_produto&validade=passei&tipo=descricao&valor=")
+with open("config.txt") as config_file:
+    url, login, password = config_file.readlines()
+
+driver.get(url)
 
 pages_number = len(driver.find_elements_by_class_name("indicePagina")) + 1
 
 driver.find_element_by_id("areaLoginCliente").click()
-driver.find_element_by_id("usuario").send_keys("0071454")
-driver.find_element_by_id("senha").send_keys("3401")
+driver.find_element_by_id("usuario").send_keys(login)
+driver.find_element_by_id("senha").send_keys(password)
 driver.find_element_by_id("btLogin").click()
 
 with open("products.txt", "w+") as products_file:
     csv_writer = csv.writer(products_file, delimiter=";")
 
     for page_index in range(pages_number):
-        driver.get(f"http://segseguranca.com.br/base.asp?currentPage={page_index + 1}&verif=ABCDEFGHI2.83321334405622POIUYT1.09861228866811&link=consulta_produto&validade=passei&tipo=descricao&valor=")
+        driver.get(f"{url}&currentPage={page_index + 1}")
 
         for product in driver.find_elements_by_class_name("cabecalhoTabelaFundoColor"):
             try:
